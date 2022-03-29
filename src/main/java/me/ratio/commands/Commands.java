47b246205cb53d4ac379cd54a8f6587d2e214510
billdims <billldims@gmail.com>
2022-03-29 20:04:37 +0200

    updates fr

--- src/main/java/me/ratio/commands/Commands.java
+++ src/main/java/me/ratio/commands/Commands.java
@@ -1,6 +1,7 @@
 package me.ratio.commands;
 
 import io.github.staudlol.util.player.PlayerUtility;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -15,9 +16,27 @@ public final class Commands extends JavaPlugin {
         this.getCommand("ping").setExecutor(new CommandExecutor() {
             @Override
             public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-                Player player = (Player) sender;
-                int latency = PlayerUtility.getPing(player);
-                player.sendMessage(ChatColor.GRAY + "Your ping is " + ChatColor.DARK_RED + latency + "ms");
+//                Player player = (Player) sender;
+//                int latency = PlayerUtility.getPing(player);
+//                player.sendMessage(ChatColor.GRAY + "Your ping is " + ChatColor.DARK_RED + latency + "ms");
+                Player target = (Player) sender;
+
+                if(args.length >= 1) {
+                    target = Bukkit.getPlayer(args[0]);
+                }
+
+                if(target == null) {
+                    sender.sendMessage(ChatColor.DARK_RED + "This player is offline");
+                    return false;
+                }
+
+                int latency = PlayerUtility.getPing(target);
+
+                if(target == (Player) sender) {
+                    sender.sendMessage(ChatColor.GRAY + "Your ping is " + ChatColor.DARK_RED + latency + "ms");
+                } else {
+                    sender.sendMessage(ChatColor.GRAY + target.getName() + "'s ping is" + ChatColor.DARK_RED + latency + "ms");
+                }
                 return false;
             }
         });
